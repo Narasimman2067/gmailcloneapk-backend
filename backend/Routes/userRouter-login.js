5697ae641532b2cfdbd9851b28288b56fda5411f
Narasimman2067 <narasimmamech2067@gmail.com>
2023-04-09 23:43:08 +0530

    commit

--- backend/Routes/userRouter-login.js
+++ backend/Routes/userRouter-login.js
@@ -12,34 +12,6 @@ const loginRouter=express.Router()
 
 loginRouter.post("/login",loginUser)
 
-// const loginRouter =express.Router();
 
-
-// loginRouter.post("/",async (req,res)=>{
-//     try {
-//         const user=await User.findone({emial:req.body.email})
-//         if(user)
-//         {
-//             return res.status(400).json({message:"invalid user credentials"})
-//         }
-//         const validatePassword=await bcrypt.compare(
-//             req.body.password,
-//             user.password
-//         )
-//         if(!validatePassword){
-//             return res.status(400).json({message:"invalid password"})
-//         }
-        
-//         const Token=generateAuthToken(user._id)
-
-
-//         res.status(200).send({message:"loged in succesfully",Token})
-   
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({message:"Internal server error"})
-        
-//     }
-// })
 
 export default loginRouter;
