a048363bfd2ebadf81e2bc43f898862dee87e355
Narasimman2067 <narasimmamech2067@gmail.com>
2023-04-10 09:07:40 +0530

    c

--- backend/Controllers/emailbody-controllers.js
+++ backend/Controllers/emailbody-controllers.js
@@ -54,7 +54,7 @@ export const postEmail= async(req, res) => {
         name : req.body.name,
         subject : req.body.subject,
         message : req.body.message,
-        user:req.User._id
+        user:req.user.ObjectId(_id)
          }
            ).save();
  try {
